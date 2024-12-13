14177035ec5cfe2ef736a25d616b4bfc5257f63d
Oshima Uta <e245726@ie.u-ryukyu.ac.jp>
2024-12-13 14:11:46 +0900

    2th commit for report5

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         String str = null;
-        System.out.println(str.length());
+        try{
+            System.out.println(str.length());
+        }catch(Throwable e){
+            System.out.println("NullPointerExceptionエラーが発生しました"+e.getMessage());
+        }
     }
+
 }
